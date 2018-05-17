d596352a315fdc8afaba06ec8d196d392b28afbf
mitanshu001 <mitanshu.sheo@iitg.ernet.in>
2018-05-17 09:31:19 +0000

    Required field check done

--- js/ground_book.js
+++ js/ground_book.js
@@ -1,40 +1,5 @@
 document.getElementById('date').valueAsDate = new Date();
 
-// function generate_table() {
-//   // get the reference for the body
-//   var body = document.getElementsByTagName("body")[0];
-//
-//   // creates a <table> element and a <tbody> element
-//   var tbl = document.createElement("table");
-//   var tblBody = document.createElement("tbody");
-//
-//   // creating all cells
-//   for (var i = 0; i < 2; i++) {
-//     // creates a table row
-//     var row = document.createElement("tr");
-//
-//     for (var j = 0; j < 2; j++) {
-//       // Create a <td> element and a text node, make the text
-//       // node the contents of the <td>, and put the <td> at
-//       // the end of the table row
-//       var cell = document.createElement("td");
-//       var cellText = document.createTextNode("cell in row "+i+", column "+j);
-//       cell.appendChild(cellText);
-//       row.appendChild(cell);
-//     }
-//
-//     // add the row to the end of the table body
-//     tblBody.appendChild(row);
-//   }
-//
-//   // put the <tbody> in the <table>
-//   tbl.appendChild(tblBody);
-//   // appends <table> into <body>
-//   body.appendChild(tbl);
-//   // sets the border attribute of tbl to 2;
-//   tbl.setAttribute("border", "2");
-// }
-
 function createTable() {
 
     alert('Success! Your rquest is submitted.');
@@ -50,6 +15,9 @@ function createTable() {
             tbody += '<td>' + String(dep.name) + '</td>';
             tbody += '<td>' + String(dep.value) + '</td>';
             tbody += '</tr>\n';
+        }
+        if (response[i].name == "submit") {
+            continue;
         }
         tbody += '<tr>';
         tbody += '<td>' + String(response[i].name) + '</td>';
@@ -67,20 +35,13 @@ function createTable() {
 
     var new_head = '<h2 class="titl">Sports Ground Online Booking </h2><br> '
 
-    //     addPageMarkup();
-    //
-    // var cd=document.getElementById( "containerDiv" );
-    //
-    // cd.insertBefore(document.createTextNode("Container text"), cd.firstChild);
-    //
-    // document.getElementById( "innerDiv" ).innerHTML = "Inner Div text";
-
 
     document.getElementById('fill').innerHTML = new_head;
     document.getElementById('wrapper').innerHTML = head1 + theader + tbody + tfooter;
     var body = document.getElementsByTagName("body")[0];
     var mydiv = document.createElement("div");
     mydiv.classList.add('resubmit');
+    wrapper.classList.add('jumbotron');
     var aTag = document.createElement('a');
     aTag.setAttribute('href', "index.html");
     aTag.innerHTML = "Resubmit the form";
@@ -88,34 +49,3 @@ function createTable() {
     body.appendChild(mydiv);
     return false;
 }
-
-
-
-
-// function show_response() {
-//
-//    console.log('start');
-//
-//    var response= document.querySelectorAll('input');
-//
-//    var table_start='<table border="1">\n'
-//
-//    var table_body='';
-//
-//    for(var i=0;i<response.length-1;++i){
-//        table_body += '<tr>';
-//        table_body+='<td>'+ String(response[i].name) + '</td>';
-//        table_body+='<td>'+ String(response[i].value) + '</td>';
-//        table_body += '</tr>\n';
-//    }
-//    var table_end='</table>'
-//    var insert_display = table_start+table_body+table_end;
-//    console.log(insert_display);
-//    document.getElementById('display').innerHtml= table_start+table_body+table_end;
-//     // var tbl = document.querySelector("#display");
-//      // tbl.appendChild(insert_display);
-//      // var tbl = document.querySelector("table");
-//      // tbl.setAttribute("border", "2");
-//    console.log('end');
-//     alert ('Success! Your rquest is submitted.');
-// }
